0974c02bfbe22d1db3e3695df4e32abead25e04f
Alondaar <Alondaar@gmail.com>
2024-11-13 19:37:51 -0600

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SMT_X = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+SMT_X.abilities = {
+  str: 'SMT_X.Ability.Str.long',
+  dex: 'SMT_X.Ability.Dex.long',
+  con: 'SMT_X.Ability.Con.long',
+  int: 'SMT_X.Ability.Int.long',
+  wis: 'SMT_X.Ability.Wis.long',
+  cha: 'SMT_X.Ability.Cha.long',
+};
+
+SMT_X.abilityAbbreviations = {
+  str: 'SMT_X.Ability.Str.abbr',
+  dex: 'SMT_X.Ability.Dex.abbr',
+  con: 'SMT_X.Ability.Con.abbr',
+  int: 'SMT_X.Ability.Int.abbr',
+  wis: 'SMT_X.Ability.Wis.abbr',
+  cha: 'SMT_X.Ability.Cha.abbr',
+};
